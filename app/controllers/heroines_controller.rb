9edff1fdc44b8e7bf1568ac4a8a1d6de8f0280a9
afhare <alexandra.f.hare@gmail.com>
2019-06-14 10:05:17 -0500

    basic MVP

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,36 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show]
+  
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @errors = @heroine.errors.full_messages
+      @powers = Power.all
+      render :new
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,5 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates :super_name, uniqueness: true
+    accepts_nested_attributes_for :power
 end
