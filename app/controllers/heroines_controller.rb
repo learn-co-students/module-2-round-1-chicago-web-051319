9c2eafbde7219e315ce1a5a9ea4b39533abce3ea
github username <github email address>
2019-06-14 10:44:52 -0500

    code challenge submission

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    find_heroine
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @errors=@heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,8 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :name, presence: true
+  validates :super_name, presence: true
+  validates :super_name, uniqueness: true
+
 end
